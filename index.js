82e40ae5e4a859548b760e8f7a1d8ee7befb61cb
Felipe <felipezeni06@gmail.com>
2021-09-15 19:14:46 -0300

    Projetos da Aula 1

---
+++ index.js
@@ -0,0 +1,21 @@
+// const arrayVazia = [];
+
+// console.log(arrayVazia);
+// console.log(arrayVazia.length);
+
+
+
+// const arrayVirgula = [ , , , ]
+// console.log(arrayVirgula[0]);
+// console.log(arrayVirgula[1]);
+// console.log(arrayVirgula[2]);
+// console.log(arrayVirgula.length);
+
+
+// ARRAY ESPARSO
+
+const arrayVazia = [,,,];
+console.log(arrayVazia.length);
+arrayVazia.push(50);
+console.log(arrayVazia);
+console.log(arrayVazia.length);
